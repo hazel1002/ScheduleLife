189cf32dc2e5b855ab765607aa39ca228d39d408
Hazel <mimihuang1002tw@gmail.com>
2024-09-01 23:28:52 +0800

    feat: add connectionString and new APIs

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,50 @@
-var express = require('express');
-var router = express.Router();
+import express from 'express';
+const router = express.Router();
+import db from "../db/conn.js"
+import { ObjectId } from 'mongodb';
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+// Mock database
+const users = [
+  {
+    first_name: 'John',
+    last_name: 'Doe',
+    email: 'johndoe@example.com',
+  },
+  {
+    first_name: 'Alice',
+    last_name: 'Smith',
+    email: 'alicesmith@example.com',
+  },
+];
+
+// Getting the list of users
+router.get('/', async (req, res) => {
+    //res.send(users);
+    let collection = await db.collection("PlayList");
+    let results = await collection.find({})
+      .limit(50)
+      .toArray();
+    res.send(results).status(200);
+})
+
+// Get a single user
+router.get("/:id", async (req, res) => {
+  let collection = await db.collection("PlayList");
+  let query = {_id: new ObjectId(req.params.id)};
+  let result = await collection.findOne(query);
+  if (!result) res.send("Not found").status(404);
+  else res.send(result).status(200);
 });
 
-module.exports = router;
+// Add a user
+router.post('/', async (req, res) => {
+  let collection = await db.collection("PlayList");
+  let newUser = req.body;
+  newUser.date = new Date();
+  let result = await collection.insertOne(newUser);
+  res.send(result).status(204);
+}) 
+
+
+
+export default router
